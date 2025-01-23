44ed08190d8c40294a1ca2d5c91a8521cead69ce
shadcn <m@shadcn.com>
2025-01-23 22:41:09 +0400

    chore: fix lint errors

--- components/sink/navigation-menu-demo.tsx
+++ components/sink/navigation-menu-demo.tsx
@@ -62,7 +62,7 @@ export function NavigationMenuDemo() {
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <a
+                  <Link
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-linear-to-b from-muted/50 to-muted p-6 no-underline outline-hidden focus:shadow-md"
                     href="/"
                   >
@@ -73,7 +73,7 @@ export function NavigationMenuDemo() {
                       Beautifully designed components built with Radix UI and
                       Tailwind CSS.
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href="/docs" title="Introduction">
--- components/sink/tabs-demo.tsx
+++ components/sink/tabs-demo.tsx
@@ -23,7 +23,8 @@ export function TabsDemo() {
           <CardHeader>
             <CardTitle>Account</CardTitle>
             <CardDescription>
-              Make changes to your account here. Click save when you're done.
+              Make changes to your account here. Click save when you&apos;re
+              done.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
@@ -46,7 +47,8 @@ export function TabsDemo() {
           <CardHeader>
             <CardTitle>Password</CardTitle>
             <CardDescription>
-              Change your password here. After saving, you'll be logged out.
+              Change your password here. After saving, you&apos;ll be logged
+              out.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
